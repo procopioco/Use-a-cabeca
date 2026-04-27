6662337127bf0a250767cbe11977b7504919e479
procopico <guilhermbarbosa3@gmail.com>
2026-04-27 16:51:32 -0300

    aprendendo

---
+++ incremento-decremento/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 4; i++) {
+
+            for(int j = 4; j > 2; j++) {
+                System.out.println(i +" " +j);
+            }
+
+            if ( )
+        }
+    }
+}
